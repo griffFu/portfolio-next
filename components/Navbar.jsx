2615b1aaecada5d8fb6f3132a4b90cc084bb16b3
griffFu <gfulto.9@gmail.com>
2022-11-26 16:06:05 -0600

    setting up next js project

---
+++ components/Navbar.jsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+function Navbar() {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/logo3.png"
+          alt="/"
+          width="125"
+          height="50"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <div>
+              <Image
+                src="/../public/assets/logo3.png"
+                alt="/"
+                height="100"
+                width="100"
+              ></Image>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
