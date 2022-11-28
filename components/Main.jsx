8a21689679a4c21c38e269b06be91e36ba81346d
GriffFu <gfulto.9@gmail.com>
2022-11-27 22:37:26 -0600

    adding reactive buttons for socials in main

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
+import {useState,React} from 'react'
+import {FaLinkedinIn} from 'react-icons/fa'
+import {AiFillGithub, AiOutlineMail} from 'react-icons/Ai'
 
 function Main() {
+  //hover states Linked
+  const [isHoveringL, setIsHoveredL] = useState(false);
+  const onMouseEnterL = () => setIsHoveredL(true);
+  const onMouseLeaveL = () => setIsHoveredL(false);
+  //hover states github
+  const [isHoveringG, setIsHoveredG] = useState(false);
+  const onMouseEnterG = () => setIsHoveredG(true);
+  const onMouseLeaveG = () => setIsHoveredG(false);
+
+
+
   return (
     <div className="h-screen w-full text-center">
       <div className="mx-auto max-w-[1240px] w-full h-full p-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600">
-            Link and Build?
-          </p>
+          
           <h1>
-            Heyo, its <span className="text-green-900">Griff</span>
+            I'm Griffin
           </h1>
+          <h1 className='text-green-900'>
+            <span className="text-green-900"> a full-stack developer</span>
+          </h1>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            Scroll to see some cool stuff
+          </p>
+          <div className='flex justify-between items-center py-5 m-auto max-w-[50%]'>
+            <div className='fill-green-900 rounded-full shadow-lg shadow-gray-600 p-3 sm:p-5 cursor-pointer hover:scale-110'
+            onMouseEnter={onMouseEnterG} 
+            onMouseLeave={onMouseLeaveG}>
+            <AiFillGithub className= {isHoveringG ? 'fill-green-900':''} size={24} />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-600 p-3 sm:p-5 cursor-pointer hover:scale-110' 
+            onMouseEnter={onMouseEnterL} onMouseLeave={onMouseLeaveL}>
+            <FaLinkedinIn className={isHoveringL ? 'fill-blue-600':''} size={24}/>
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-600 p-3 sm:p-5 cursor-pointer hover:scale-110'>
+            <AiOutlineMail size={24}/>
+            </div>
+          
         </div>
+        </div>
+       
       </div>
     </div>
   )
